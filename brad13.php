60395538b76eb52dfa49d339964470d43ca2892a
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-28 15:52:53 +0800

    07281552

--- brad13.php
+++ brad13.php
@@ -1,7 +1,16 @@
 <?php
-    // Loop
-    for ($i = 0; $i < 10; $i++){
+    // Loop => 小看 for
+    $i = 0;
+    for (test1() ; $i < 10 ; drawLine()){
         echo "{$i}<br />";
+        $i++;
+    }
+
+    function test1(){
+        echo 'Brad<br />';
+    }
+    function drawLine(){
+        echo '<hr />';
     }
 
 ?>
