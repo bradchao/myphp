5ca156605354b1bd030c49d35eeac0a77ec718c5
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-28 16:31:32 +0800

    07281630

--- brad99.php
+++ brad99.php
@@ -1,63 +1,23 @@
+<h1>九九乘法表</h1>
+<hr />
 <table border="1" width="100%">
-    <tr>
-        <td>
-            <?php
-                for ($i = 1; $i <= 9; $i++){
-                    $r = 2 * $i;
-                    echo "2 x {$i} = {$r}<br />";
-                }
-            ?>
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>
-    <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-    </tr>    
+    <?php
+    define("ROW", 2);
+    define("COL", 4);
+    define("START", 2);
+
+    for ($k = 0; $k < ROW; $k++) {
+        echo '<tr>';
+        for ($j = START; $j <= START + COL - 1; $j++) {
+            $newj = $j + $k * COL;
+            echo '<td>';
+            for ($i = 1; $i <= 9; $i++) {
+                $r = $newj * $i;
+                echo "{$newj} x {$i} = {$r}<br />\n";
+            }
+            echo '</td>';
+        }
+        echo '</tr>';
+    }
+    ?>
 </table>
