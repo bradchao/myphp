d1e32bbdff2d9ffcc33d78bacc18e27dbe32ea88
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-28 10:30:59 +0800

    07281030

--- brad07.php
+++ brad07.php
@@ -1,7 +1,12 @@
 <meta charset="UTF-8" />
-<form>
-    <input />
-    +
-    <input />
-    <input type="submit" value="=" />
+<script>
+    function add(){}
+</script>
+<hr />
+<form action="brad08.php">
+    <input type="number" id="x" name="x"/>
+    <span>+</span>
+    <input type="number" id="y" name="y"/>
+    <input type="button" onclick="add()" value="=(JS)" />
+    <input type="submit" value="=(PHP)" />
 </form>
