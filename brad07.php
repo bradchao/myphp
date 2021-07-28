13cd41f0ac4edb23535df0c6002f4b8bbde21ef9
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-28 13:44:31 +0800

    07281344

--- brad07.php
+++ brad07.php
@@ -1,8 +1,20 @@
 <?php 
     $result = '';
     if (isset($_GET['x']) && isset($_GET['y'])){
+        $op = $_GET['op'];
         $x = $_GET['x']; $y = $_GET['y'];
-        $result = $x + $y;
+
+        if ($op == 1){
+            $result = $x + $y;
+        }else if ($op == 2){
+            $result = $x - $y;
+        }else if ($op == 3){
+            $result = $x * $y;
+        }else if ($op == 4){
+            $result = $x / $y;
+        }else{
+            $result = "";
+        }
     }
 ?>
 <meta charset="UTF-8" />
@@ -10,9 +22,15 @@
     function add(){}
 </script>
 <hr />
+<!-- %2B + , %2F / => URL編碼 -->
 <form method="get">
     <input type="number" id="x" name="x" value="<?php echo $x?>" />
-    <span>+</span>
+    <select name="op">
+        <option value="1">+</option>
+        <option value="2">-</option>
+        <option value="3">x</option>
+        <option value="4">/</option>
+    </select>
     <input type="number" id="y" name="y" value="<?php echo $y?>" />
     <input type="button" onclick="add()" value="=(JS)" />
     <input type="submit" value="=(PHP)" />
