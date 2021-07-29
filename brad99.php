86a49eda50f29097825cd02d1b340ba0c780dc66
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-29 09:13:24 +0800

    07290913

--- brad99.php
+++ brad99.php
@@ -2,15 +2,21 @@
 <hr />
 <table border="1" width="100%">
     <?php
-    define("ROW", 2);
+    define("ROW", 4);
     define("COL", 4);
     define("START", 2);
 
     for ($k = 0; $k < ROW; $k++) {
         echo '<tr>';
-        for ($j = START; $j <= START + COL - 1; $j++) {
+        for ($j = START; $j < START + COL; $j++) {
             $newj = $j + $k * COL;
-            echo '<td>';
+
+            if (($j+$k) % 2 == 0){
+                echo '<td bgcolor="yellow">';
+            }else{
+                echo '<td bgcolor="pink">';
+            }
+            
             for ($i = 1; $i <= 9; $i++) {
                 $r = $newj * $i;
                 echo "{$newj} x {$i} = {$r}<br />\n";
