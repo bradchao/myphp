3f1c2e490dea2afb2cb052fcca8ecd72c0a5a02f
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-29 11:53:10 +0800

    07291153

--- brad19.php
+++ brad19.php
@@ -1,13 +1,22 @@
 <?php
     // 模擬擲骰子100次 => 統計各點出現的次數
     $p = array(1 => 0,0,0,0,0,0); // $p[1] = 0 ~ $p[6] = 0
-    for ($i = 0; $i < 100; $i++){
-        $point = rand(1,6);
-        $p[$point]++;
+    for ($i = 0; $i < 1000000; $i++){
+        $point = rand(1,9);
+        
+        //$p[ $point>6 ? $point-3 : $point ]++;
+
+        if ($point > 6){
+            $p[$point-3]++;
+        }else{
+            $p[$point]++;
+        }
     }
 
-    foreach($p as $point => $times){
-        echo "{$point}點出現{$times}次<br />";
+    echo "<hr />";
+    foreach($p as $key => $value){
+        echo "{$key}點出現{$value}次<br />";
     }
+
 
 ?>
