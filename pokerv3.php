08593accb7ff808f071ad1ad2d47520aa36c211c
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-07-30 11:37:03 +0800

    07301137

--- pokerv3.php
+++ pokerv3.php
@@ -2,18 +2,24 @@
     $poker = range(0,51);
     shuffle($poker);
 
-    foreach($poker as $v){
-        echo $v . '<br />';
-    }
-    echo "---<br />";
-
     $players = [[],[],[],[]];
     foreach($poker as $i => $card){
         $players[$i % 4][ (int)($i / 4) ] = $card;
     }
-
-    foreach($players[1] as $v){
-        echo "{$v}<br />";
-    }
-
 ?>
+<table border="1" width="100%">
+    <?php
+        foreach($players as $player){
+            echo '<tr>';
+            foreach($player as $card){
+                echo "<td>{$card}</td>";
+            }
+            echo '</tr>';
+        }
+    ?>
+</table>
+
+
+
+
+
