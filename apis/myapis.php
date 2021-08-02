768c4a0cb3cc31046455c45710de8a3abe44859c
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-02 13:57:02 +0800

    08021356

---
+++ apis/myapis.php
@@ -0,0 +1,6 @@
+<?php
+    // f(x,y) = 2x + y
+    function fxy($x, $y){
+        return 2*$x + $y;
+    }
+?>
--- brad31.php
+++ brad31.php
@@ -1,11 +1,14 @@
 <?php
     include "bradapis.php";
+    include "apis/myapis.php";
     
     if (checkTWId('A123456789')){
         echo 'OK';
     }else{
         echo 'XX';
     }
+
+    echo fxy(2,3);
 
 
 ?>
