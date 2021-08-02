d534238501282dee8a51318e001a6f441335fbe7
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-02 09:15:24 +0800

    08020915

---
+++ brad24.php
@@ -0,0 +1,15 @@
+<?php
+    foreach( $_SERVER as $key => $value){
+        if (gettype($value) == 'array'){
+            echo "{$key} => ";
+            foreach($value as $kk => $vv){
+                echo "{$kk} : {$vv}<br />";
+            }
+            echo "===<br />";
+        }else{
+            echo "{$key} : {$value}<br />";
+        }
+    }
+
+
+?>
