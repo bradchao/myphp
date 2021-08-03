8baa204042d068d3c07383a6ab4f4611e8234917
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-03 09:09:00 +0800

    08030908

--- brad34.php
+++ brad34.php
@@ -1,13 +1,25 @@
+
+<table border="1" width="100%" />
+    <tr>
+        <th>Filename</th>
+        <th>File or Dir</th>
+        <th>size</th>
+        <th>Modify Time</th>
+    </tr>
 <?php
     $fp = @opendir('/Applications/MAMP/htdocs/myphp') or exit('Server Busy v2');
     if ($fp){
         while ($row = readdir($fp)){
-            echo $row . '<br />';
+            echo '<tr>';
+            echo "<td>{$row}</td>";
+            echo "<td></td>";
+            echo "<td></td>";
+            echo "<td></td>";
+            echo '</tr>';
         }
     }else{
         echo 'XX';
     }
     closedir($fp);
-
-
 ?>
+</table>
