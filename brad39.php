12a13f88c1e22f860d80ba7b3e030436c20ed0a1
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-03 21:13:41 +0800

    08032020

--- brad39.php
+++ brad39.php
@@ -1,5 +1,11 @@
 <?php
-    $fp = fopen('https://www.iii.org.tw/', 'r') or die();
+    $context = stream_context_create(array('ssl'=>array(
+        'verify_peer' => true,
+        'cafile' => 'ca-bundle.crt'
+    )));
+    libxml_set_streams_context($context);
+
+    $fp = fopen('https://www.iii.org.tw/', 'r', false, $context) or die();
 
     $i = 0;
     while ($line = @fgetss($fp)){
