9fa0f273f511b57bb36d35a5551e9895bcd29828
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-03 11:22:58 +0800

    08031122

--- brad37.php
+++ brad37.php
@@ -1,12 +1,29 @@
+
+<table border="1" width="100%">
 <?php
     $fp = @fopen("maskdata.csv", "r") or die("Server Busy");
 
-    while ( $line = trim(@fgets($fp))){
-        echo "{$line}<br />";
-    }
+    $line1 = trim(@fgets($fp));
+    $headers = explode(",", $line1);
+    echo '<tr>';
+    echo "<th>$headers[1]</th>";
+    echo "<th>$headers[2]</th>";
+    echo "<th>$headers[4]</th>";
+    echo "<th>$headers[5]</th>";
+    echo '</tr>';
 
+
+    while ( $line = trim(@fgets($fp))){
+        $row = explode(",", $line);
+        echo '<tr>';
+        echo "<td>$row[1]</td>";
+        echo "<td>$row[2]</td>";
+        echo "<td>$row[4]</td>";
+        echo "<td>$row[5]</td>";
+        echo '</tr>';
+    }
 
     fclose($fp);
 
-
 ?>
+</table>
