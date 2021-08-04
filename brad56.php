411ae4b78cb843dd55fd9353ee3c8cd445fcf1c9
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-04 15:59:09 +0800

    08041559

--- brad56.php
+++ brad56.php
@@ -2,12 +2,15 @@
     $mysqli = new mysqli('127.0.0.1','root','root','class', 3306);
     $mysqli->set_charset("utf8");
 
-    $sql = "DELETE FROM students WHERE cID > ?";
+    $sql = "UPDATE students SET ch =?, eng=?, math=? WHERE cID = ?";
     $stmt = $mysqli->prepare($sql);
     //$stmt = mysqli_prepare($mysqli, $sql);
 
-    $delId = 10;
-    $stmt->bind_param('i', $delId);
+    $updateId = 10;
+    $ch = rand(50,80);
+    $eng = rand(50,80);
+    $math = rand(50,80);
+    $stmt->bind_param('iiii', $ch,$eng,$math,$updateId );
     $stmt->execute();
 
     echo $mysqli->errno . '<br />';
