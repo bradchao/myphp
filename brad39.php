abc8222f3686e1417a4b0856f45c2fba0750140b
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-04 09:05:09 +0800

    08040905

--- brad39.php
+++ brad39.php
@@ -6,6 +6,7 @@
     libxml_set_streams_context($context);
 
     $fp = fopen('https://www.iii.org.tw/', 'r', false, $context) or die();
+    // $fp = fopen('https://www.bradchao.com/', 'r');
 
     $i = 0;
     while ($line = @fgetss($fp)){
@@ -14,3 +15,9 @@
 
     fclose($fp);
 ?>
+
+老師我想請教一下39檔案 即使複製老師的程式碼也一樣報錯 找不到問題在哪邊
+來自 洪宇涓 至所有人:    下午 4:46 
+Warning: fopen(): SSL operation failed with code 1. OpenSSL Error messages: error:14090086:SSL routines:ssl3_get_server_certificate:certificate verify failed in /Applications/MAMP/htdocs/myPHP/jen39.php on line 2
+Warning: fopen(): Failed to enable crypto in /Applications/MAMP/htdocs/myPHP/jen39.php on line 2
+Warning: fopen(https://www.iii.org.tw/): failed to open stream: operation failed in /Applications/MAMP/htdocs/myPHP/jen39.php on line 2
