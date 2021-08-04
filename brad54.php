6ffde38b7014e1e7c64f3415b5ac7012ac618dfd
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-04 15:03:27 +0800

    08041503

--- brad54.php
+++ brad54.php
@@ -1,5 +1,20 @@
 <?php
     $mysqli = new mysqli('127.0.0.1','root','root','class', 3306);
-    var_dump($mysqli);
+    $mysqli->set_charset("utf8");
+
+    $sql = "INSERT INTO students (cName,cSex,cBirthday) VALUES (?,?,?)";
+    $stmt = $mysqli->prepare($sql);
+    //$stmt = mysqli_prepare($mysqli, $sql);
+
+    $cName = 'Brad';
+    $cSex = 'M';
+    $cBirthday = '1999-01-02';
+    $stmt->bind_param('sss', $cName, $cSex, $cBirthday);
+    if ($stmt->execute()){
+        echo "OK";
+    }else{
+        echo "XX";
+    }
+
 
 ?>
