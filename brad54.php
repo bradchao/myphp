621d9684c7f2183d39501f70cefd8139a066bf98
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-04 15:17:04 +0800

    08041516

--- brad54.php
+++ brad54.php
@@ -2,19 +2,20 @@
     $mysqli = new mysqli('127.0.0.1','root','root','class', 3306);
     $mysqli->set_charset("utf8");
 
-    $sql = "INSERT INTO students (cName,cSex,cBirthday) VALUES (?,?,?)";
+    $sql = "INSERT INTO students (cName,cSex,cBirthday,ch,eng,math) VALUES (?,?,?,?,?,?)";
     $stmt = $mysqli->prepare($sql);
     //$stmt = mysqli_prepare($mysqli, $sql);
 
-    $cName = 'Brad';
-    $cSex = 'M';
-    $cBirthday = '1999-01-02';
-    $stmt->bind_param('sss', $cName, $cSex, $cBirthday);
-    if ($stmt->execute()){
-        echo "OK";
-    }else{
-        echo "XX";
-    }
+    $var1 = '資策會v3';
+    $var2 = 'F';
+    $var3 = '1999-01-02';
+    $var4 = rand(0,100);
+    $var5 = rand(0,100);
+    $var6 = rand(0,100);
+    $stmt->bind_param('sssiii', $var1,$var2,$var3,$var4,$var5,$var6);
+    $stmt->execute();
 
+    echo $mysqli->errno . '<br />';
+    echo $mysqli->error . '<br />';
 
 ?>
