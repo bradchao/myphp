775eb9443df3152ef059309fc136c35909190071
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-04 16:24:00 +0800

    08041623

---
+++ brad58.php
@@ -0,0 +1,12 @@
+<?php
+    $mysqli = new mysqli('127.0.0.1','root','root','class', 3306);
+    $mysqli->set_charset("utf8");
+
+    $result = $mysqli->query('SELECT * FROM students');
+
+    while ($rows = $result->fetch_array()){
+        echo "{$rows['cID']} : {$rows['cName']} : {$rows['ch']}<br />";
+    }
+
+
+?>
