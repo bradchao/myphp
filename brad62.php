b7e7ca0c0bbb55294b3d1a3ace4f14c96e4a91b5
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-05 12:03:59 +0800

    08051203

--- brad62.php
+++ brad62.php
@@ -1,5 +1,6 @@
 <script>
     const xhttp = new XMLHttpRequest();
+    var isCheckOK = false;
 
     function test(){
         xhttp.onreadystatechange = callback;
@@ -15,30 +16,38 @@
     }
 
     function checkAccount(){
+        isCheckOK = false;
         let account = document.getElementById('account').value;
         console.log("brad63.php?account=" + account);
 
         xhttp.onreadystatechange = afterCheck;
         xhttp.open("GET", "brad63.php?account=" + account);
         xhttp.send();
+        console.log("check send");
     }
 
     function afterCheck(){
         if (this.readyState == 4 && this.status == 200) {
+                mesg = xhttp.responseText.trim();
                 document.getElementById("mesg").innerHTML =
-                xhttp.responseText;
+                mesg;
+                if (mesg == 0){
+                    isCheckOK = true;
+                }
+                console.log("check back:" + mesg.length);
         }
     }
 
-    function test2(){
-        return true;
+    function checkForm(){
+        console.log("checkForm..." + isCheckOK);
+        return isCheckOK;
     }
 
 </script>
 
 Register Form
 <hr />
-<form method="post" action="brad64.php" onsubmit="return test2();">
+<form method="post" action="brad64.php" onsubmit="return checkForm();">
     Account: <input type="text" id='account' name="account" onblur="checkAccount()"/> 
     <span id='mesg'></span> <br />
     Password: <input type="password" name="passwd" /><br />
