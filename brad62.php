21bffff59584915291994c91f185bdbac1b21aaa
Brad Chao <28331110+mybradtw@users.noreply.github.com>
2021-08-05 10:58:55 +0800

    08051058

--- brad62.php
+++ brad62.php
@@ -1,3 +1,25 @@
+<script>
+    const xhttp = new XMLHttpRequest();
+
+    function test(){
+        xhttp.onreadystatechange = callback;
+        xhttp.open("GET", "ajax.php");
+        xhttp.send();
+    }
+
+    function callback(){
+        if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("here").innerHTML =
+                xhttp.responseText;
+        }
+    }
+
+    // function test0(){
+    //     setInterval(test, 1000);
+    // }
+
+</script>
+
 Register Form
 <hr />
 <form method="post" action="brad62.php">
@@ -6,3 +28,8 @@ Register Form
     Email: <input type="text" name="email" /><br />
     <input type="submit" value="Register" />
 </form>
+<hr />
+<div id='lottery'></div>
+<input type="button" value="test" onclick="test()" /><br />
+<div id='here'></div>
+
